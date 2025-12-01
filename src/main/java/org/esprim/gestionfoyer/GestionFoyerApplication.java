c503304d7d74d7b12f97369ae274c817706153a4
moame <moamedsalahsaoudi123@gmail.com>
2025-12-01 14:36:23 +0100

    Add scheduling support and update server context path in application properties

--- src/main/java/org/esprim/gestionfoyer/GestionFoyerApplication.java
+++ src/main/java/org/esprim/gestionfoyer/GestionFoyerApplication.java
@@ -3,8 +3,10 @@ package org.esprim.gestionfoyer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GestionFoyerApplication {
 
     public static void main(String[] args) {
